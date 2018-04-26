af8a6fe50694f16966c04a2fda4d79e94667221f
kentyku <kentyku@box>
2018-04-26 20:06:19 +0400

    Поправил  комментарии

--- src/avtomorfnie/Avtomorfnie.java
+++ src/avtomorfnie/Avtomorfnie.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Use and copying for commercial purposes
+ * only with the author's permission
  */
 package avtomorfnie;
 
